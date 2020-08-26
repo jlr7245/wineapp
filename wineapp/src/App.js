d9e1e7e9f5098fd91c5eef02265bcbe04f340e5c
J Silverstein <jess.pebble@gmail.com>
2020-08-26 12:04:19 -0400

    add components and make some styles

--- wineapp/src/App.js
+++ wineapp/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Nav from './components/Nav';
+import WineBox from './components/WineBox';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
+      <main>
+        <Nav />
+        <WineBox />
+      </main>
     </div>
   );
 }
