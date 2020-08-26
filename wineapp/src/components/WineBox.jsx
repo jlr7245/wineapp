1fff56857fdf41170b34ae942a5aac17c11faf81
J Silverstein <jess.pebble@gmail.com>
2020-08-26 12:53:48 -0400

    stuff

--- wineapp/src/components/WineBox.jsx
+++ wineapp/src/components/WineBox.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const WineBox = () => {
+  const [wineList, setWineList] = useState([]);
+  useEffect(() => {
+    fetch('http://myapi-profstream.herokuapp.com/api/1e02af/wines')
+      .then((res) => res.json())
+      .then((res) => {
+        setWineList(res);
+      });
+  }, []);
   return (
     <section>
-      <h1>The WINEBOX!!!!!!</h1>
+      {wineList.length ? (
+        wineList.map((wine) => (
+          <div key={wine.id}>
+            <img src={wine.picture} />
+            <h3>
+              {wine.name}, {wine.year}
+            </h3>
+            <p>
+              Made with {wine.grapes} grapes grown in {wine.region},{' '}
+              {wine.country}
+            </p>
+            <p>{wine.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </section>
-  )
-}
+  );
+};
 
 export default WineBox;
