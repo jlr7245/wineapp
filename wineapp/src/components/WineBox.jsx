8faa32c9ff9f270b710e4a1d860895d23f6b1246
J Silverstein <jess.pebble@gmail.com>
2020-08-26 12:38:31 -0400

    formatting

--- wineapp/src/components/WineBox.jsx
+++ wineapp/src/components/WineBox.jsx
@@ -4,23 +4,31 @@ const WineBox = () => {
   const [wineList, setWineList] = useState([]);
   useEffect(() => {
     fetch('http://myapi-profstream.herokuapp.com/api/1e02af/wines')
-      .then(res => res.json())
-      .then(res => {
-        console.log(res);
+      .then((res) => res.json())
+      .then((res) => {
         setWineList(res);
-      })
+      });
   }, []);
   return (
     <section>
-      {wineList.length ? wineList.map(wine => (
-        <div key={wine.id}>
-          <h3>{wine.name}, {wine.year}</h3>
-          <p>Made with {wine.grapes} grapes grown in {wine.region}, {wine.country}</p>
-          <p>{wine.description}</p>
-        </div>
-      )) : <p>Loading...</p>} 
+      {wineList.length ? (
+        wineList.map((wine) => (
+          <div key={wine.id}>
+            <h3>
+              {wine.name}, {wine.year}
+            </h3>
+            <p>
+              Made with {wine.grapes} grapes grown in {wine.region},{' '}
+              {wine.country}
+            </p>
+            <p>{wine.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </section>
-  )
-}
+  );
+};
 
 export default WineBox;
